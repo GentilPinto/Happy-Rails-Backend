c883c9a0aa2a90b2d2146bf59de5ebde91b429bb
GentilPinto <gentil.junior.gp@gmail.com>
2020-10-20 22:00:14 +0100

    create: orphanage model and migration

---
+++ db/migrate/20201020205856_create_orphanages.rb
@@ -0,0 +1,15 @@
+class CreateOrphanages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orphanages do |t|
+      t.string :name
+      t.decimal :latitude
+      t.decimal :longitude
+      t.text :about
+      t.text :instructions
+      t.string :opening_hours
+      t.boolean :open_on_weekends
+
+      t.timestamps
+    end
+  end
+end
